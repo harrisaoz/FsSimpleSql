09d7f05e0a0e467e83aa192fd918bde4664c396e
Anthony Harris <vibe@ah.id.au>
2023-02-02 11:02:43 +0000

    Provide a simple means to create and assign values to DB Parameters.

---
+++ FsSimpleSql/Parameters.fs
@@ -0,0 +1,10 @@
+﻿module FsSimpleSql.Parameters
+
+open System.Data.Common
+open Microsoft.Data.SqlClient
+
+let newParameter name dbType : DbParameter = SqlParameter(name, dbType)
+
+let inline withValue value (parameter: DbParameter) =
+    parameter.Value <- value
+    parameter
