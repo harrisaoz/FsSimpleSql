d4f7f78821867139a7f536d8a5516fd5cc909745
Anthony Harris <vibe@ah.id.au>
2023-02-03 10:29:46 +0000

    Elaborate on the Parameters module - breaking change (newParameter).
    Warning: the order of parameters to Parameters.newParameter changes in this version from newParameter name dbType to newParameter dbType name (to facilitate cleaner partial application for typed parameter constructor functions).

--- FsSimpleSql/Parameters.fs
+++ FsSimpleSql/Parameters.fs
@@ -3,7 +3,51 @@
 open System.Data.Common
 open Microsoft.Data.SqlClient
 
-let newParameter name dbType : DbParameter = SqlParameter(name, dbType)
+let fixParameterName (name: string) =
+    if name.StartsWith("@") then name else $"@{name}"
+
+/// <summary>
+/// Create a new DbParameter of the given type and '@' name.
+/// </summary>
+///
+/// <remarks>
+/// Type-specific utility function via partial application:
+/// <code>
+/// let newIdParameter = newParameter SqlDbType.Int
+/// let personIdParam = newIdParameter "person_id"
+/// let hatIdParam = newIdParameter "hat_id"
+/// </code>
+/// Chaining:
+/// <code>
+/// newParameter SqlDbType.Int "maxLengthMm" | withValue 10
+/// </code>
+/// </remarks>
+///
+/// <param name="name">The name of the parameter.  The name may or may not
+/// include the initial '@' character - if not present, it is added before
+/// creating the parameter.</param>
+/// <param name="dbType">The column type of the parameter. See
+/// <see cref="System.Data.Common.SqlDbType">SqlDbType</see></param>
+let newParameter dbType (name: string) : DbParameter =
+    SqlParameter(fixParameterName name, dbType)
+
+let havingSize size (parameter: DbParameter) =
+    parameter.Size <- size
+    parameter
+
+/// <summary>
+/// Equivalent to <c>newParameter type name |> havingSize size</c>.
+/// </summary>
+let newSizedParameter dbType size name : DbParameter =
+    SqlParameter(fixParameterName name, dbType, size)
+
+let havingPrecision precision (parameter: DbParameter) =
+    parameter.Precision <- precision
+    parameter
+
+let havingScale scale (parameter: DbParameter) =
+    parameter.Scale <- scale
+    parameter
 
 let inline withValue value (parameter: DbParameter) =
     parameter.Value <- value
