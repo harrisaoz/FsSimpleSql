12529b7fb98c6a4ea241b7c1f8850f729f135436
Anthony Harris <vibe@ah.id.au>
2023-02-03 15:46:04 +0000

    New feature: template for constant memory sequential, synchronised DML actions based on results of a SQL query.
    The template provides for an optional non-database (external) action to feed into the DML operation. However, the result data from the external action is currently ignored - only the fact of whether it succeeded or not is taken into account.

--- FsSimpleSql/Facades/SyncSeqApply.fs
+++ FsSimpleSql/Facades/SyncSeqApply.fs
@@ -1,4 +1,40 @@
 ﻿module FsSimpleSql.Facades.SyncSeqApply
 
-let map query command (commandConnection, queryConnection) =
-    1
+open System.Data.Common
+open FsCombinators.Core
+open FsSimpleSql
+
+type ExternalAction<'a, 'b> =
+    | Noop of result: Result<'b, string>
+    | ExternalFunction of fn: ('a -> Result<'b, string>)
+
+let noop = Noop(Result.Ok None)
+
+let map
+    (initQuery: DbCommand -> DbCommand)
+    (resultSetAdapter: DbDataReader -> 'a)
+    (externalAction: ExternalAction<'a, 'e>)
+    (initDbCommand: DbCommand -> DbCommand)
+    (rebindCommand: 'a -> DbCommand -> DbCommand)
+    (commandConnection, queryConnection)
+    =
+    let dmlStatement: DbCommand =
+        Statement.newStatement commandConnection
+        |> initDbCommand
+        |> Statement.prepareStatement
+
+    let each (record: 'a) : Result<int, string> =
+        match externalAction with
+        | ExternalFunction externalCommand -> externalCommand record
+        | Noop result -> result
+        |> Result.bind (fun _ ->
+            dmlStatement
+            |> rebindCommand record
+            |> C (Tx.inTransaction commandConnection) Exec.executeDml)
+
+    Statement.newStatement queryConnection
+    |> initQuery
+    |> Statement.prepareStatement
+    |> Exec.executeQuery
+    |> Records.enumerateResultSet resultSetAdapter
+    |> Seq.map (fun r -> (r, each r))
